a7f907d22fb4955b1ad66697318e067c658090cf
Alperen Demirci <alperndmrcii@gmail.com>
2023-03-14 19:35:08 +0300

    XML files and Multiple Classes

---
+++ src/Gun04/_02_Intro.java
@@ -0,0 +1,15 @@
+package Gun04;
+
+import org.testng.annotations.Test;
+
+public class _02_Intro {
+    @Test
+    void Intro1_Test1() {
+        System.out.println("Intro1 -> Test1 ");
+    }
+
+    @Test
+    void Intro1_Test2() {
+        System.out.println("Intro1 -> Test2 ");
+    }
+}
