608df55cabee7e65e02fdb08211b11d9433a25b5
Alperen Demirci <alperndmrcii@gmail.com>
2023-03-14 21:55:08 +0300

    Parameters value

--- src/Gun04/_05_SearchFunctionality.java
+++ src/Gun04/_05_SearchFunctionality.java
@@ -4,20 +4,22 @@ import Utility.BaseDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class _05_SearchFunctionality extends BaseDriver {
     @Test
-    void SearchFuntion(){
+    @Parameters("aranacakKelime")
+    void SearchFuntion(String text){
         WebElement searchInput= driver.findElement(By.cssSelector("[name='search']"));
-        searchInput.sendKeys("mac");
+        searchInput.sendKeys(text);
         WebElement searchButton=driver.findElement(By.cssSelector("[class='btn btn-default btn-lg'"));
         searchButton.click();
 
         List<WebElement> captions=driver.findElements(By.cssSelector("div[class='caption']>h4"));
         for (WebElement e : captions)
-            Assert.assertTrue(e.getText().toLowerCase().contains("mac"));
+            Assert.assertTrue(e.getText().toLowerCase().contains(text));
     }
 }
