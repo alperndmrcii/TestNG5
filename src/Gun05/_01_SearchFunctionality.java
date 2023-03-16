08c610101b06e34c3d1421a7040fa23247a47969
Alperen Demirci <alperndmrcii@gmail.com>
2023-03-16 15:53:20 +0300

    CrossBrowser Test

---
+++ src/Gun05/_01_SearchFunctionality.java
@@ -0,0 +1,26 @@
+package Gun05;
+
+import Utility.BaseDriver;
+import Utility.BaseDriverParameter;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class _01_SearchFunctionality extends BaseDriverParameter {
+    @Test
+    @Parameters("aranacakKelime")
+    void SearchFuntion(String text){
+        WebElement searchInput= driver.findElement(By.cssSelector("[name='search']"));
+        searchInput.sendKeys(text);
+        WebElement searchButton=driver.findElement(By.cssSelector("[class='btn btn-default btn-lg'"));
+        searchButton.click();
+
+        List<WebElement> captions=driver.findElements(By.cssSelector("div[class='caption']>h4"));
+        for (WebElement e : captions)
+            Assert.assertTrue(e.getText().toLowerCase().contains(text));
+    }
+}
